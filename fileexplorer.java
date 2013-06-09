e0d3c9bb8dbcde7e13b69b7828d1b46d995d2dad
ArjunBala <arjunbala0711@gmail.com>
2013-06-09 08:59:05 +0530

    Second Commit

--- fileexplorer.java
+++ fileexplorer.java
@@ -3,12 +3,14 @@ package com.example.fileexplorer;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -18,12 +20,19 @@ public class fileexplorer extends ListActivity {
 	private List<String> path = null;
 	private String root="/";
 	private TextView myPath;
-	
+	File f = new File(root);
+	File[] files = f.listFiles();
+	Button name;
+	Button size;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name=(Button)findViewById(R.id.sortbyname);
+        size=(Button)findViewById(R.id.sortbysize);
+        name.setOnClickListener(sort_name);
+        size.setOnClickListener(sort_size);
         myPath = (TextView)findViewById(R.id.path);
         getDir(root);
     }
@@ -35,8 +44,7 @@ public class fileexplorer extends ListActivity {
     	item = new ArrayList<String>();
     	path = new ArrayList<String>();
     	
-    	File f = new File(dirPath);
-    	File[] files = f.listFiles();
+    	
     	
     	if(!dirPath.equals(root))
     	{
@@ -59,11 +67,56 @@ public class fileexplorer extends ListActivity {
     				item.add(file.getName());
     	}
 
+    	
+    	
     	ArrayAdapter<String> fileList =
     		new ArrayAdapter<String>(this, R.layout.row, item);
     	setListAdapter(fileList);
+    	
     }
-
+    
+    View.OnClickListener sort_name = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			int i,j;
+			for(i=0;i<files.length;i++)
+			{
+				for(j=i;j<files.length;j++)
+				{
+					if(strcmp(files[j],files[j+1]>0))
+					{
+						file t=files[j];
+						file[j]=file[j+1];
+						file[j+1]=t;
+					}
+				}
+			}
+				
+			}
+			
+		};
+		
+		View.OnClickListener sort_size= new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				int i,j;
+				for(i=0;i<files.length;i++)
+				{
+					for(j=0;j<files.length;j++)
+					{
+						if(files[j].size()>files[j+1].size())
+						{
+							file t= files[j];
+							file[j]=file[j+1];
+							file[j+1]=t;
+						}
+					}
+				}
+				
+			}
+		};
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		
@@ -96,14 +149,13 @@ public class fileexplorer extends ListActivity {
 				.setPositiveButton("OK", 
 						new DialogInterface.OnClickListener() {
 							
-							@Override
 							public void onClick(DialogInterface dialog, int which) {
 								
 							}
-				}).show();
-		}
+				}.show();
+		
 	}
-}
+
 						
 		
 	
